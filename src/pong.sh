52fddb54e138385d1205daa4ab11f7149e040639
boxofbytes <77898210+boxofbytes@users.noreply.github.com>
2021-10-11 17:30:08 +0200

    Fix spacing

--- src/pong.sh
+++ src/pong.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##################################################
-# 			Pong shell game by ByteBox       	       #      
+# 	   Pong shell game by ByteBox	 	 #      
 ##################################################
 
 # variables
