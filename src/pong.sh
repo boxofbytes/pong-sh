4be7bc20f25a19aae116aecb855455de8846a981
boxofbytes <77898210+boxofbytes@users.noreply.github.com>
2021-10-12 17:33:16 +0200

    Bigger update, check description
    Optimized the game by drawing the background once, and only clearing/printing the paddles or ball.
    Cleaned up code by putting everything into functions and then just calling them with or without arguments.

--- src/pong.sh
+++ src/pong.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 ##################################################
-# 			Pong shell game by ByteBox       	 #      
+#          Pong shell game by ByteBox            #     
 ##################################################
 
+############################################################################################################################################
 # variables
-CHARACTER="."
+
+BACKGROUND_CHARACTER="."
 PADDLE_CHARACTER="#"
 BALL_CHARACTER="@"
 PADDLE_HEIGHT=10
@@ -17,39 +19,26 @@ BALL_Y_POS=12
 BALL_X_MOV=1
 BALL_Y_MOV=0
 
-# clear screen
-# clear
+############################################################################################################################################
+# functions
 
-# input stuff
-stty -icanon -echo
-
-# game loop
-while :
-do
+# function for filling background with background character
+fill_background () {
 	printf "\033[0;0f"			# ANSI sequence to move cursor to top left corner
 	for y in {1..25..1}			# y screen loop
 	do
 		for x in {1..80..1}		# x screen loop
 		do
-			# comparisons for which character to put 
-			if [ $y -le $(( PADDLE_HEIGHT + PADDLE_1_HEIGHT )) ] && [ $y -gt $PADDLE_1_HEIGHT ] && [ $x -le $PADDLE_WIDTH ]; then
-				printf "%c" "$PADDLE_CHARACTER"
-			elif [ $y -le $(( PADDLE_HEIGHT + PADDLE_2_HEIGHT )) ] && [ $y -gt $PADDLE_2_HEIGHT ] && [ $x -gt $(( 80 - PADDLE_WIDTH )) ]; then
-				printf "%c" "$PADDLE_CHARACTER"
-			elif [ $y -eq $BALL_Y_POS ] && [ $x -eq $BALL_X_POS ]; then
-				printf "%c" "$BALL_CHARACTER"
-			else 
-				printf "%c" "$CHARACTER"
-			fi
+			# put background character
+			printf "%c" "$BACKGROUND_CHARACTER"
 		done
 		printf "\n"
-	done
+	done	
+}
 
-	# read one character
-	read -n1 -t 0.01;
-
-	# input control stuff
-	if [ "$REPLY" = "s" ] && [ $PADDLE_1_HEIGHT -le $((24 - PADDLE_HEIGHT)) ]; then
+# function for processing the given input
+input_control () {
+	if [ "$REPLY" = "s" ] && [ $PADDLE_1_HEIGHT -le $((25 - PADDLE_HEIGHT)) ]; then
 		 (( PADDLE_1_HEIGHT += 1 ))
 	fi
 	if [ "$REPLY" = "w" ] && [ $PADDLE_1_HEIGHT -ge 1 ]; then
@@ -58,11 +47,14 @@ do
 	if [ "$REPLY" = "e" ] && [ $PADDLE_2_HEIGHT -ge 1 ]; then
 		 (( PADDLE_2_HEIGHT -= 1 ))
 	fi
-	if [ "$REPLY" = "d" ] && [ $PADDLE_2_HEIGHT -le $((24 - PADDLE_HEIGHT)) ]; then
+	if [ "$REPLY" = "d" ] && [ $PADDLE_2_HEIGHT -le $((25 - PADDLE_HEIGHT)) ]; then
 		 (( PADDLE_2_HEIGHT += 1 ))
 	fi
 
-	# update ball direction
+}
+
+# function for updating the direction the ball is going in
+update_ball_direction () {
 	if [ $BALL_X_POS -ge $(( 80 - PADDLE_WIDTH )) ] && [ $BALL_Y_POS -ge $PADDLE_2_HEIGHT ] && [ $BALL_Y_POS -le $(( PADDLE_2_HEIGHT + PADDLE_HEIGHT )) ]; then
 		BALL_X_MOV=0
 	elif [ $BALL_X_POS -ge 80 ]; then 
@@ -79,18 +71,82 @@ do
 	elif [ $BALL_Y_POS -le 1 ]; then 
 		BALL_Y_MOV=1
 	fi
+}
 
-	# update ball pos
+# function for updating the ball's position
+update_ball_position () {
 	if [ $BALL_X_MOV -eq 0 ]; then
 		(( BALL_X_POS -= 1 ))
 	elif [ $BALL_X_MOV -eq 1 ]; then
 		(( BALL_X_POS += 1 ))
 	fi
+	
 	if [ $BALL_Y_MOV -eq 0 ]; then
 		(( BALL_Y_POS -= 1 ))
 	elif [ $BALL_Y_MOV -eq 1 ]; then
 		(( BALL_Y_POS += 1 ))
 	fi
+}
+
+# function for moving the terminal cursor and printing the ball
+print_ball () {
+	printf "\033[%d;%df" "$BALL_Y_POS" "$BALL_X_POS" 
+	printf "%c" "$1"
+}
+
+# function for moving the terminal cursor and printing the paddles
+print_paddles () {
+	printf "\033[%d;0f" "$PADDLE_1_HEIGHT"
+	for y in {1..10..1}			# y paddle loop
+	do
+		for x in {1..3..1}					# x paddle loop
+		do
+			# put paddle character
+			printf "%c" "$1"
+		done
+	printf "\n"
+	done
+
+	printf "\033[%d;78f" "$PADDLE_2_HEIGHT"
+	for y in {1..10..1}			# y paddle loop
+	do
+		for x in {1..3..1}					# x paddle loop
+		do
+			# put paddle character
+			printf "%c" "$1"
+		done
+		printf "\033[%d;78f" "$((y + PADDLE_2_HEIGHT))"
+	done
+}
+
+game_init () {
+	clear
+	stty -icanon -echo
+	fill_background
+}
+
+############################################################################################################################################
+# main game loop
+
+game_init
+
+# game loop
+while :
+do
+
+	print_ball $BALL_CHARACTER
+	print_paddles $PADDLE_CHARACTER
+	
+	# read one character
+	read -n1 -t 0.1;
+
+	print_ball $BACKGROUND_CHARACTER
+	print_paddles $BACKGROUND_CHARACTER
+	input_control
+	update_ball_direction
+	update_ball_position
 done
+
 printf "Game over!\n" 
-##################################################
+stty -canon -echo
+############################################################################################################################################
