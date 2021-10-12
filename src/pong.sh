fd0c202d2489d6a5ee1dfe52e0cb5c95b1d4d56f
boxofbytes <77898210+boxofbytes@users.noreply.github.com>
2021-10-12 19:31:52 +0200

    Added colors, made game adjust to terminal size

--- src/pong.sh
+++ src/pong.sh
@@ -7,49 +7,64 @@
 ############################################################################################################################################
 # variables
 
-BACKGROUND_CHARACTER="."
+BACKGROUND_CHARACTER='.'
 PADDLE_CHARACTER="#"
 BALL_CHARACTER="@"
-PADDLE_HEIGHT=10
-PADDLE_WIDTH=3
-PADDLE_1_HEIGHT=7
-PADDLE_2_HEIGHT=7
-BALL_X_POS=40
-BALL_Y_POS=12
+SCREEN_WIDTH=$(tput cols)
+SCREEN_HEIGHT=$(tput lines)
+PADDLE_HEIGHT=$((SCREEN_HEIGHT / 3))
+PADDLE_WIDTH=$((SCREEN_WIDTH / 25))
+BALL_X_POS=$((SCREEN_WIDTH / 2))
+BALL_Y_POS=$((SCREEN_HEIGHT / 2))
 BALL_X_MOV=1
 BALL_Y_MOV=0
+PADDLE_1_HEIGHT=$((SCREEN_HEIGHT / 2 - PADDLE_HEIGHT / 2 + 2))
+PADDLE_2_HEIGHT=$((SCREEN_HEIGHT / 2 - PADDLE_HEIGHT / 2 + 2))
+BACKGROUND_COLOR=90
+PADDLE_1_COLOR=91
+PADDLE_2_COLOR=94
+BALL_COLOR=96
 
 ############################################################################################################################################
 # functions
 
+# function for resetting terminal color
+reset_color () {
+	printf "\033[0m"
+}
+
+print_color() {
+	printf "\033[0;%dm" "$1"
+}
+
 # function for filling background with background character
 fill_background () {
 	printf "\033[0;0f"			# ANSI sequence to move cursor to top left corner
-	for y in {1..25..1}			# y screen loop
-	do
-		for x in {1..80..1}		# x screen loop
-		do
+	print_color $BACKGROUND_COLOR
+	for (( y=1; y<=SCREEN_HEIGHT; ++y)); do
+		for (( x=1; x<=SCREEN_WIDTH; ++x)); do
 			# put background character
 			printf "%c" "$BACKGROUND_CHARACTER"
 		done
-		if  [ $y -lt 25 ]; then
+		if  [ $y -lt $SCREEN_HEIGHT ]; then
 			printf "\n"
 		fi
 	done	
+	reset_color
 }
 
 # function for processing the given input
 input_control () {
-	if [ "$REPLY" = "s" ] && [ $PADDLE_1_HEIGHT -le $((25 - PADDLE_HEIGHT)) ]; then
+	if [ "$REPLY" = "s" ] && [ $PADDLE_1_HEIGHT -le $((SCREEN_HEIGHT - PADDLE_HEIGHT)) ]; then
 		 (( PADDLE_1_HEIGHT += 1 ))
 	fi
-	if [ "$REPLY" = "w" ] && [ $PADDLE_1_HEIGHT -ge 1 ]; then
+	if [ "$REPLY" = "w" ] && [ $PADDLE_1_HEIGHT -ge 2 ]; then
 		 (( PADDLE_1_HEIGHT -= 1 ))
 	fi
-	if [ "$REPLY" = "e" ] && [ $PADDLE_2_HEIGHT -ge 1 ]; then
+	if [ "$REPLY" = "e" ] && [ $PADDLE_2_HEIGHT -ge 2 ]; then
 		 (( PADDLE_2_HEIGHT -= 1 ))
 	fi
-	if [ "$REPLY" = "d" ] && [ $PADDLE_2_HEIGHT -le $((25 - PADDLE_HEIGHT)) ]; then
+	if [ "$REPLY" = "d" ] && [ $PADDLE_2_HEIGHT -le $((SCREEN_HEIGHT - PADDLE_HEIGHT)) ]; then
 		 (( PADDLE_2_HEIGHT += 1 ))
 	fi
 
@@ -57,14 +72,14 @@ input_control () {
 
 # function for updating the direction the ball is going in
 update_ball_direction () {
-	if [ $BALL_X_POS -ge $(( 80 - PADDLE_WIDTH )) ] && [ $BALL_Y_POS -ge $PADDLE_2_HEIGHT ] && [ $BALL_Y_POS -le $(( PADDLE_2_HEIGHT + PADDLE_HEIGHT )) ]; then
+	if [ $BALL_X_POS -ge $(( SCREEN_WIDTH - PADDLE_WIDTH )) ] && [ $BALL_Y_POS -ge $PADDLE_2_HEIGHT ] && [ $BALL_Y_POS -le $(( PADDLE_2_HEIGHT + PADDLE_HEIGHT )) ]; then
 		BALL_X_MOV=0
 	fi
 	if [ $BALL_X_POS -le $(( 1 + PADDLE_WIDTH )) ] && [ $BALL_Y_POS -ge $PADDLE_1_HEIGHT ] && [ $BALL_Y_POS -le $(( PADDLE_1_HEIGHT + PADDLE_HEIGHT )) ]; then
 		BALL_X_MOV=1
 	fi
 
-	if [ $BALL_Y_POS -ge 25 ]; then 
+	if [ $BALL_Y_POS -ge $SCREEN_HEIGHT ]; then 
 		BALL_Y_MOV=0
 	elif [ $BALL_Y_POS -le 1 ]; then 
 		BALL_Y_MOV=1
@@ -88,35 +103,37 @@ update_ball_position () {
 
 # function for moving the terminal cursor and printing the ball
 print_ball () {
+	print_color $2
 	printf "\033[%d;%df" "$BALL_Y_POS" "$BALL_X_POS" 
 	printf "%c" "$1"
+	reset_color
 }
 
 # function for moving the terminal cursor and printing the paddles
 print_paddles () {
 	printf "\033[%d;0f" "$PADDLE_1_HEIGHT"
-	for y in {1..10..1}			# y paddle loop
-	do
-		for x in {1..3..1}					# x paddle loop
-		do
+	
+	print_color $2
+	for (( y=1; y<=PADDLE_HEIGHT; ++y)); do
+		for (( x=1; x<=PADDLE_WIDTH; ++x)); do
 			# put paddle character
 			printf "%c" "$1"
 		done
-		if  [ $y -lt 10 ]; then
+		if  [ $y -lt $PADDLE_HEIGHT ]; then
 			printf "\n"
 		fi
 	done
-
-	printf "\033[%d;78f" "$PADDLE_2_HEIGHT"
-	for y in {1..10..1}			# y paddle loop
-	do
-		for x in {1..3..1}					# x paddle loop
-		do
+	
+	print_color $3
+	printf "\033[%d;%df" "$PADDLE_2_HEIGHT" "$((SCREEN_WIDTH + 1 - PADDLE_WIDTH))"
+	for (( y=1; y<=PADDLE_HEIGHT; ++y)); do
+		for (( x=1; x<=PADDLE_WIDTH; ++x)); do
 			# put paddle character
 			printf "%c" "$1"
 		done
-		printf "\033[%d;78f" "$((y + PADDLE_2_HEIGHT))"
+		printf "\033[%d;%df" "$((y + PADDLE_2_HEIGHT))" "$((SCREEN_WIDTH + 1 - PADDLE_WIDTH))"
 	done
+	reset_color
 }
 
 game_init () {
@@ -133,30 +150,30 @@ game_init
 # game loop
 while :
 do
+	print_ball $BALL_CHARACTER $BALL_COLOR
+	print_paddles $PADDLE_CHARACTER $PADDLE_1_COLOR $PADDLE_2_COLOR
 
-	print_ball $BALL_CHARACTER
-	print_paddles $PADDLE_CHARACTER
-	
-	# read one character
-	read -n1 -t 0.1;
-
-	print_ball $BACKGROUND_CHARACTER
-	print_paddles $BACKGROUND_CHARACTER
-	input_control
-	update_ball_direction
-	
 	# check if ball is out of bounds
-	if [ $BALL_X_POS -ge 80 ]; then 
+	if [ $BALL_X_POS -ge $SCREEN_WIDTH ]; then 
 		break
 	fi
 	if [ $BALL_X_POS -le 1 ]; then 
 		break
 	fi
 	
+	# read one character
+	read -n1 -t 0.1;
+	
+	print_ball $BACKGROUND_CHARACTER $BACKGROUND_COLOR
+	print_paddles $BACKGROUND_CHARACTER $BACKGROUND_COLOR $BACKGROUND_COLOR
+	
+	input_control
+	update_ball_direction
 	update_ball_position
 done
 
-printf "\033[26;0f"
+printf "\033[%d;%df" "$((SCREEN_HEIGHT / 2 + 1))" "$((SCREEN_WIDTH / 2 - 5))"
 printf "Game over!\n" 
+printf "\033[%d;%d0f" "$((SCREEN_HEIGHT))"
 stty icanon echo
 ############################################################################################################################################
